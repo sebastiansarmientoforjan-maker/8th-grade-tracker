e5f5d6cfb68d1c4f0170800cd637a5c630adea01
sebastiansarmientoforjan-maker <sebastian.sarmiento.forjan@gmail.com>
2025-08-23 20:30:37 -0400

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,1 +1,22 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCaHoev9SS07-M5EoKE-E3xVxfHCS2Qz64",
+  authDomain: "th-grade-tracker.firebaseapp.com",
+  projectId: "th-grade-tracker",
+  storageBucket: "th-grade-tracker.firebasestorage.app",
+  messagingSenderId: "728386315853",
+  appId: "1:728386315853:web:738bc67430ccbae0e69e04",
+  measurementId: "G-DCC5HDDH6J"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
